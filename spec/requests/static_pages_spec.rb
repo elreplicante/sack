a4ca80b105d15aa33aea7dafb4dc989c2fe1b48e
Sergio Revilla <sergio@unmundotecnologico.org>
2013-12-07 22:27:07 +0100

    Refactoring home page specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have content 'sack'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('sack')
     end
 
     it "has the rigth title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('sack')
     end
   end
