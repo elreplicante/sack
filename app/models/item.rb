6826dd909fbeb9300955f9c1cf7352cddd4e9850
Sergio Revilla <sergio@unmundotecnologico.org>
2013-12-08 01:30:58 +0100

    Validating url on item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item
   field :url, type: String
   field :name, type: String
   field :description, type: String
+
+  validates_presence_of :url
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Item do
-  
+  it { should validate_presence_of(:url) }
 end
