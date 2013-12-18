2b8cedef6b9523fbd68707c5a5cd5337de8b7429
Sergio Revilla <sergio@unmundotecnologico.org>
2013-12-18 22:38:27 +0100

    Search matches ALL keywords

--- config/initializers/mongoid_search.rb
+++ config/initializers/mongoid_search.rb
@@ -1,6 +1,6 @@
 Mongoid::Search.setup do |config|
   ## Default matching type. Match :any or :all searched keywords
-  config.match = :any
+  config.match = :all
 
   ## If true, an empty search will return all objects
   config.allow_empty_search = false
