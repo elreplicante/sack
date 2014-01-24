db1a65751855fcf36e9400994266f88338f3d020
alejandrodob <alejandrodob@gmail.com>
2014-01-24 13:53:43 +0100

    Added validation of URLs

--- features/item.feature
+++ features/item.feature
@@ -1,7 +1,13 @@
 Feature: Add item
 
-  Scenario: Bad url
+  Scenario: Invalid url
     Given I am in the app
-    When I fill it with "badurl"
+    When I fill it with "Bad url"
     And I click "Add"
-    Then I should see "Bad url"
+    Then I should see "Bad url" within ".alert"
+
+  Scenario: Valid url
+    Given I am in the app
+    When I fill it with "http://www.as.com"
+    And I click "Add"
+    Then I should see "URL added to the sack!" within ".alert"
--- spec/javascripts/AppSpec.js
+++ spec/javascripts/AppSpec.js
@@ -1,5 +1,39 @@
 describe("App", function() {
-  it("should be working", function() {
-    expect(true).toBeTruthy();
+
+  var html2test='<div id="html2test" style="visibility:hidden"></div>';
+  var input = '<input id="url-input" type="url"></input>';
+  var invalidInput = 'hola';
+  var validInput = "http://www.as.com";
+  var url;
+
+  beforeEach(function(){
+    $('body').append(html2test);
+    $('#html2test').append(input);
+    url = $('#url-input');
   });
+
+  describe("When adding a new URL", function() {
+    it("shows an error if invalid URL", function() {
+      spyOn(App, '_showError');
+      url.val(invalidInput);
+      console.log(url.val());
+      App.validateURL();
+      expect(App._showError).toHaveBeenCalled();
+    });
+
+    it("saves the item if URL is valid", function() {
+      spyOn(App, "saveItem");
+      url.val(validInput);
+      console.log(url.val());
+      App.validateURL(validInput);
+      expect(App.saveItem).toHaveBeenCalled();
+    });
+  });
+  
+  afterEach(function() {
+    $('#html2test').remove();
+  });
+
 });
+
+
